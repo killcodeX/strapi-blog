b9e78c0645d28a3dcecfac389ae36326cb4fa5a1
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-10-02 19:32:28 +0530

    updated link

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,3 @@
 module.exports = ({ env }) => ({
-    url: env("HEROKU_URL"),
+    url: env("postgres://gptulvqtcmmyle:ee24551436d4f1610c58450e538fe6a5f2ea5bcfffcb16ee23f13358c86eefdc@ec2-100-24-169-249.compute-1.amazonaws.com:5432/dae17a6nobhkd0"),
   });
