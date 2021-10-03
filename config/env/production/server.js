5c9bb47805ec270d3da21d102c4fe7b515e75cd9
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-10-03 06:35:05 +0530

    cloudnary setup

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,5 @@
 module.exports = ({ env }) => ({
-    url: env("postgres://gptulvqtcmmyle:ee24551436d4f1610c58450e538fe6a5f2ea5bcfffcb16ee23f13358c86eefdc@ec2-100-24-169-249.compute-1.amazonaws.com:5432/dae17a6nobhkd0"),
-  });
+  url: env(
+    "postgres://gptulvqtcmmyle:ee24551436d4f1610c58450e538fe6a5f2ea5bcfffcb16ee23f13358c86eefdc@ec2-100-24-169-249.compute-1.amazonaws.com:5432/dae17a6nobhkd0"
+  ),
+});
